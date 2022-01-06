45f7fa9e1297c9bf933d94544279127beba1f72b
ames <game.ekkarat@gmail.com>
2022-01-06 16:33:51 +0700

    Update 06.01.2022

--- vehicle/models/vehicle_management.py
+++ vehicle/models/vehicle_management.py
@@ -10,26 +10,24 @@ class VehicleManagement(models.Model):
     _description = 'Vehicle Management'
     _inherit = 'mail.thread', 'mail.activity.mixin'
 
-    name = fields.Char(string='Order Number', track_visibility='onchange', default=None)
-    partner_id = fields.Many2one('fleet.vehicle', string='Model', required=True, track_visibility='onchange')
-    driver_id = fields.Many2one('res.users', string='Driver', required=True, track_visibility='onchange')
-    objective = fields.Char(string='Objective', required=True, track_visibility='onchange')
-    location = fields.Text(string='Location', required=True, track_visibility='onchange')
-    distance = fields.Float(string='Distance', required=True, track_visibility='onchange')
-    travel_date = fields.Datetime(string='Travel Date', required=True, track_visibility='onchange',
-                                  default=datetime.now().strftime('%Y-%m-%d %H:%M'))
-    return_date = fields.Datetime(string='Return Date', required=True, track_visibility='onchange',
-                                  default=datetime.now().strftime('%Y-%m-%d %H:%M'))
-    oil_budget = fields.Selection([('country_budget', 'งบประมาณแผ่นดิน'),
-                                   ('income_statement', 'งบรายได้'),
-                                   ('other', 'อื่นๆ')], required=True, track_visibility='onchange')
-    n_teacher = fields.Integer(string='Teachers Number', required=True, track_visibility='onchange')
-    n_student = fields.Integer(string='Students Number', required=True, track_visibility='onchange')
-    note = fields.Text(string='Description', track_visibility='onchange')
+    name = fields.Char(string='Order Number', required=True, track_visibility='onchange', default='NEW', readonly='1')
     user_id = fields.Many2one('res.users', string='User', required=True, track_visibility='onchange',
                               default=lambda self: self.env.company)
-
-    # state_id = fields.Many2one('maintenance', string='State', required=True)
+    objective = fields.Char(string='Objective', required=True, track_visibility='onchange')
+    teacher = fields.Integer(string='Number of Teachers', required=True, track_visibility='onchange')
+    student = fields.Integer(string='Number of Students', required=True, track_visibility='onchange')
+    model_id = fields.Many2one('fleet.vehicle', string='Model', required=True, track_visibility='onchange')
+    image_128 = fields.Image(related='model_id.image_128', readonly=True)
+    location = fields.Text(string='Location', required=True, track_visibility='onchange')
+    distance = fields.Float(string='Distance', required=True, track_visibility='onchange')
+    budget = fields.Selection([('budget', 'งบประมาณแผ่นดิน'), ('statement', 'งบรายได้'), ('other', 'อื่นๆ')],
+                              required=True, track_visibility='onchange')
+    start_date = fields.Datetime(string='Start Date', required=True, track_visibility='onchange',
+                                 default=datetime.now().strftime('%Y-%m-%d %H:%M'))
+    end_date = fields.Datetime(string='End Date', required=True, track_visibility='onchange',
+                               default=datetime.now().strftime('%Y-%m-%d %H:%M'))
+    driver_id = fields.Many2one('res.users', string='Driver', required=True, track_visibility='onchange')
+    # state = fields.Many2one('maintenance', string='State', required=True)
 
     @api.model
     def create(self, vals):
