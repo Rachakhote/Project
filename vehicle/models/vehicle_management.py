100ac89b372d95990d79dd418cc33aa7d18dd7f2
ames <game.ekkarat@gmail.com>
2022-01-25 09:25:14 +0700

    New update

--- vehicle/models/vehicle_management.py
+++ vehicle/models/vehicle_management.py
@@ -10,20 +10,21 @@ class VehicleManagement(models.Model):
     _description = 'Vehicle Management'
     _inherit = 'mail.thread', 'mail.activity.mixin'
 
-    name = fields.Char(string='Order Number', tracking=True, default='NEW', readonly=True)
-    user_id = fields.Many2one('res.users', string='User', required=True, tracking=True,
+    name = fields.Char(string='หมายเลข', tracking=True, default='NEW', readonly=True)
+    user_id = fields.Many2one('res.users', string='ผู้ขอใช้', required=True, tracking=True,
                               default=lambda self: self.env.company)
     position = fields.Char(string='ตำแหน่ง', tracking=True)
     agency = fields.Char(string='หน่วยงาน', tracking=True)
-    objective = fields.Char(string='วัตถุประสงค์', tracking=True)
-    teacher_number = fields.Integer(string='จำนวนอาจารย์ผู้ร่วมเดินทาง', tracking=True)
-    student_number = fields.Integer(string='จำนวนนักศึกษาผู้ร่วมเดินทาง', tracking=True)
-    model_id = fields.Many2one('fleet.vehicle', string='Model', required=True, tracking=True)
+    objective = fields.Char(string='จุดประสงค์ในการขอใช้รถ', tracking=True)
+    teacher_number = fields.Integer(string='มีผู้ร่วมเดินทางเป็นอาจารย์', tracking=True)
+    student_number = fields.Integer(string='นักศึกษา', tracking=True)
+    model_id = fields.Many2one('fleet.vehicle', string='ประเภทรถยนต์', required=True, tracking=True)
     image_128 = fields.Image(related='model_id.image_128', readonly=True)
     location = fields.Text(string='สถานที่', tracking=True)
     distance = fields.Float(string='ระยะทางไป-กลับ', tracking=True)
-    budget = fields.Selection([('country_budget', 'งบประมาณแผ่นดิน'), ('statements', 'งบรายได้'), ('other', 'อื่นๆ')],
-                              string='ค่าน้ำมันเชื้อเพลิง', tracking=True, default='country_budget')
+    budget = fields.Selection(
+        [('budget_land', 'งบแผ่นดิน'), ('statements', 'งบรายได้'), ('other', 'งบอื่นๆ')],
+        string='งบประมาณค่าน้ำมันเชื้อเพลิง', tracking=True, default='budget_land')
     note = fields.Char(string='ระบุ', tracking=True)
     start_date = fields.Datetime(string='วันเวลาในการออกเดินทาง', tracking=True,
                                  default=datetime.now().strftime('%Y-%m-%d %H:%M'))
@@ -33,43 +34,52 @@ class VehicleManagement(models.Model):
     phone_number = fields.Char(string='เบอร์โทรศัพท์', tracking=True)
     office_number = fields.Char(string='เบอร์สำนักงาน', tracking=True)
 
+    # เจ้าหน้าที่ธุรการ
+
     record = fields.Selection(
         [('approve', 'เห็นควรอนุมัติ'), ('disapproved', 'ไม่เห็นควรอนุมัติ'), ('consider', 'โปรดพิจารณา')],
         string='บันทึก', tracking=True)
     driver_id = fields.Many2one('res.users', string='พนักงานขับรถ', tracking=True)
     attendant = fields.Char(string='พนักงานประจำรถ', tracking=True)
     authorities = fields.Many2one('res.users', string='ลงชื่อ', tracking=True)
-    approve_date = fields.Date(string='วันที่อนุมัติ', default=fields.Date.today(), tracking=True)
+    approve_date = fields.Date(string='วันที่อนุมัติ', default=fields.Date.today(), readonly=True)
+
+    # หัวหน้าฝ่ายยานพาหนะ
 
     propose = fields.Selection([('approve', 'อนุมัติ'), ('disapproved', 'ไม่อนุมัติ'), ('consider', 'โปรดพิจารณา')],
                                string='เสนอ', tracking=True)
     because = fields.Char(string='เพราะ', tracking=True)
     department_head = fields.Many2one('res.users', string='ลงชื่อ', tracking=True)
-    approve_date01 = fields.Date(string='วันที่อนุมัติ', default=fields.Date.today(), tracking=True)
+    approve_date01 = fields.Date(string='วันที่อนุมัติ', default=fields.Date.today(), readonly=True)
+
+    # ผู้อำนวยการสำนักงานอธิการบดี
 
     propose_02 = fields.Selection([('approve', 'อนุมัติ'), ('disapproved', 'ไม่อนุมัติ'), ('consider', 'โปรดพิจารณา')]
                                   , string='เสนอ', tracking=True)
     because_02 = fields.Char(string='เพราะ', tracking=True)
     office_director = fields.Many2one('res.users', string='ลงชื่อ', tracking=True)
-    approve_date02 = fields.Date(string='วันที่อนุมัติ', default=fields.Date.today(), tracking=True)
+    approve_date02 = fields.Date(string='วันที่อนุมัติ', default=fields.Date.today(), readonly=True)
+
+    # รองอธิการบดีฝ่ายบริหาร
 
     propose_03 = fields.Selection([('approve', 'อนุมัติ'), ('disapproved', 'ไม่อนุมัติ'), ('consider', 'โปรดพิจารณา')]
                                   , string='เสนอ', tracking=True)
     because_03 = fields.Char(string='เพราะ', tracking=True)
     vice_president = fields.Many2one('res.users', string='ลงชื่อ', tracking=True)
-    approve_date03 = fields.Date(string='วันที่อนุมัติ', default=fields.Date.today(), tracking=True)
+    approve_date03 = fields.Date(string='วันที่อนุมัติ', default=fields.Date.today(), readonly=True)
+
+    # อธิการบดีมหาวิทยาลัยราชภัฏบุรีรัมย์
 
     propose_04 = fields.Selection([('approve', 'อนุมัติ'), ('disapproved', 'ไม่อนุมัติ')], string='เสนอ', tracking=True)
     because_04 = fields.Char(string='เพราะ', tracking=True)
     chancellor = fields.Many2one('res.users', string='ลงชื่อ', tracking=True)
-    approve_date04 = fields.Date(string='วันที่อนุมัติ', default=fields.Date.today(), tracking=True)
+    approve_date04 = fields.Date(string='วันที่อนุมัติ', default=fields.Date.today(), readonly=True)
 
     state = fields.Selection(
         [('draft', 'ฉบับร่าง'), ('authorities', 'เจ้าหน้าที่'), ('department head', 'หัวหน้าฝ่าย'),
          ('director', 'ผู้อำนวยการ'), ('vice president', 'รองอธิการบดี'),
          ('chancellor', 'อธิการบดี'), ('cancel', 'ยกเลิก')],
         string='สถานะ', default='draft')
-    date = fields.Date(string='วันที่')
 
     @api.model
     def create(self, vals):
@@ -82,7 +92,7 @@ class VehicleManagement(models.Model):
         for data in self:
             data.write({
                 'approve_date': datetime.now().strftime('%Y-%m-%d'),
-                'state': 'authorities',
+                'state': 'authorities'
             })
             # ค้นหาข้อมุลจาก Class อื่น
             # obj = self.env['fleet.vehicle']
